b9b0c84823a2620fd63704a0492c1a571a9ea363
Zoran Regvart <zregvart@redhat.com>
2018-07-25 13:55:52 +0200

    refactor: allow no results from JUnit
    This allows for no results from JUnit, so it won't fail the build if no tests are run.

--- vars/boosterBuild.groovy
+++ vars/boosterBuild.groovy
@@ -37,7 +37,7 @@ def call() {
             try {
               sh './mvnw -B -V test'
             } finally {
-              junit('target/*-reports/*.xml')
+              junit(testResults: 'target/*-reports/*.xml', allowEmptyResults: true)
             }
           }
 
@@ -47,7 +47,7 @@ def call() {
               sh './mvnw -B -V fabric8:deploy -Popenshift'
             } finally {
               sh "${oc_home}/oc cluster down"
-              junit('target/*-reports/*.xml')
+              junit(testResults: 'target/*-reports/*.xml', allowEmptyResults: true)
             }
           }
         }
